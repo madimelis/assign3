97ff76e1881df0baf10688c19b137fedd65d4373
Madi Melis <250270@astanait.edu.kz>
2026-01-17 00:37:59 +0500

    create Patient class with fields and constructor

---
+++ src/Patient.java
@@ -0,0 +1,13 @@
+public class Patient {
+    private int id;
+    private String name;
+    private int age;
+    private int doctorId;
+
+    public Patient(int id, String name, int age, int doctorId) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.doctorId = doctorId;
+    }
+}
