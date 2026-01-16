c8c1f64063a36dc9e1b58559da47d7555fc77625
Madi Melis <250270@astanait.edu.kz>
2026-01-17 01:10:09 +0500

    add cases

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
+import java.io.IOException;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         Hospital h = new Hospital();
 
@@ -15,6 +16,42 @@ public class Main {
 
             System.out.print("Choose: ");
             int ch = sc.nextInt();
+
+            switch (ch) {
+                case 1 -> {
+                    System.out.print("Doctor name: ");
+                    String n = sc.next();
+                    System.out.print("Specialization: ");
+                    String s = sc.next();
+                    h.addDoctor(n, s);
+                }
+
+                case 2 -> h.getDoctors().forEach(System.out::println);
+
+                case 3 -> {
+                    System.out.print("Patient name: ");
+                    String n = sc.next();
+                    System.out.print("Age: ");
+                    int a = sc.nextInt();
+                    System.out.print("Doctor ID: ");
+                    int d = sc.nextInt();
+                    h.addPatient(n, a, d);
+                }
+
+                case 4 -> h.getPatients().forEach(System.out::println);
+
+                case 5 -> {
+                    System.out.print("Patient ID: ");
+                    h.deletePatient(sc.nextInt());
+                }
+
+                case 0 -> {
+                    System.out.println("Bye");
+                    return;
+                }
+            }
+        }
     }
 }
+
 
