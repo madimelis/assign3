a5e79d294ac135b46f93794ee5abc1ca3cd8d621
Madi Melis <250270@astanait.edu.kz>
2026-01-17 01:03:40 +0500

    add scanner and interactive menu to Main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Hospital h = new Hospital();
 
+        while (true) {
+            System.out.println("\n1. Add doctor");
+            System.out.println("2. Show doctors");
+            System.out.println("3. Add patient");
+            System.out.println("4. Show patients");
+            System.out.println("5. Delete patient");
+            System.out.println("0. Exit");
+
+            System.out.print("Choose: ");
+            int ch = sc.nextInt();
     }
 }
 
