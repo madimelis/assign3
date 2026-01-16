2c6b5401cb5eef36507624362cdb6dd0d614fcbf
Madi Melis <250270@astanait.edu.kz>
2026-01-17 00:43:22 +0500

    create Doctor class with fields and constructor

---
+++ src/Doctor.java
@@ -0,0 +1,11 @@
+public class Doctor {
+    private int id;
+    private String name;
+    private String specialization;
+
+    public Doctor(int id, String name, String specialization) {
+        this.id = id;
+        this.name = name;
+        this.specialization = specialization;
+    }
+}
